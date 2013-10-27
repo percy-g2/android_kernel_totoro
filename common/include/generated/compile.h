f9f457106d1eb43035648450162d69622d4a57b2
percy_g2 <gahlotpercy@gmail.com>
2013-10-27 22:30:05 +0530

    update 2 push

---
+++ common/include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 104 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#104 PREEMPT Sun Oct 27 17:00:57 IST 2013"
+#define LINUX_COMPILE_TIME "17:00:57"
+#define LINUX_COMPILE_BY "GNU"
+#define LINUX_COMPILE_HOST "percy_g2"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2009q3-68) "
